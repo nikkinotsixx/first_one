11ac395608ae13af8b4962675824e74bdf1b5205
nikkinotsixx <nikita.veremeev@gmail.com>
2018-11-13 00:30:56 +0300

    исправление замечаний

--- Lesson_1/exersice1.rb
+++ Lesson_1/exersice1.rb
@@ -1,11 +1,11 @@
-print "Как вас зовут?"
+print "Как вас зовут? "
 name = gets.chomp.capitalize!
-print "Какого вы роста?"
-height = gets.chomp.to_i
+print "Какого вы роста? "
+height = gets.to_i
 
 perfect_weight = height - 110
 
-if perfect_weight >=0
+if perfect_weight >= 0
 	puts "#{name}, ваш идеальный вес равен #{perfect_weight} кг."
 else
 	puts "Ваш вес уже оптимальный."
--- Lesson_1/exersice2.rb
+++ Lesson_1/exersice2.rb
@@ -1,7 +1,7 @@
 puts "Вычислите площадь треугольника зная его основание и высоту"
 print "Введите основание"
-base = gets.chomp.to_f
+base = gets.to_f
 print "Введите высоту"
-height = gets.chomp.to_f
-square = (1.0 / 2.0) * base * height
+height = gets.to_f
+square = 0.5 * base * height
 puts "Площадь треугольника - #{square}."
