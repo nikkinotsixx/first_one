2c44059d3bfae31bd7f3d0534c4a79ce6e45f9b1
nikkinotsixx <44784293+nikkinotsixx@users.noreply.github.com>
2018-11-12 17:38:41 +0300

    Delete exersice2.rb

--- exersice2.rb
+++
@@ -1,7 +0,0 @@
-puts "Вычислите площадь треугольника зная его основание и высоту"
-print "Введите основание"
-base = gets.chomp.to_f
-print "Введите высоту"
-height = gets.chomp.to_f
-square = (1.0 / 2.0) * base * height
-puts "Площадь треугольника - #{square}."
