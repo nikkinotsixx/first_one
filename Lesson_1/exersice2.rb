8a4962e98de954cabf494625aaf46be0d3af0883
nikkinotsixx <nikita.veremeev@gmail.com>
2018-11-14 13:39:09 +0300

    Исправление замечаний

---
+++ Lesson_1/exersice2.rb
@@ -0,0 +1,7 @@
+puts "Вычислите площадь треугольника зная его основание и высоту"
+print "Введите основание"
+base = gets.to_f
+print "Введите высоту"
+height = gets.to_f
+square = 0.5 * base * height
+puts "Площадь треугольника - #{square}."
