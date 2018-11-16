9c50b766159706a2cb1bceea030f64131fcaa537
nikkinotsixx <nikita.veremeev@gmail.com>
2018-11-16 12:21:57 +0300

    Исправления

--- Lesson_1/exersice3.rb
+++ Lesson_1/exersice3.rb
@@ -1,25 +1,22 @@
 puts "Является ли треугольник прямоугольным?"
 print "Введите сторону a "
-a = gets.chomp.to_f
+side_a = gets.chomp.to_f
 print "Введите сторону b "
-b = gets.chomp.to_f
+side_b = gets.chomp.to_f
 print "Введите сторону c "
-c = gets.chomp.to_f
+side_c = gets.chomp.to_f
 
-triangle = [a , b, c].sort!
+a, b, c = [side_a, side_b, side_c].sort
+rectangular = c**2 == a**2 + b**2
 
-rectangular = triangle[2]**2 == triangle[0]**2 + triangle[1]**2
-equilateral = a == b && a == c
-isosceles = a == b || b == c
-
-if equilateral
-	puts "Треугольник - равносторонний"
-elsif rectangular && isosceles
-	puts "Треугольник прямоугольный и равнобедренный"
+if rectangular && (a == b)
+	puts "Треугольник является прямоугольным и равнобедренным"
 elsif rectangular
-	puts "Треугольник - прямоугольный"
-elsif isosceles
-	puts "Треугольник - равнобедренный"
+	puts "Треугольник является прямоугольным"
+elsif (c == b) && (c == a) && (a == b)
+	puts "Треугольник является равносторонним и равнобедренным"
+elsif (c == b) || (c == a) || (a == b)
+	puts "Треугольник является равнобедренным"
 else
 	puts "Треугольник не прямоугольный и не равнобедренный"
 end
