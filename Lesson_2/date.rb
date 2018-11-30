905da6ef39a4394e2b97617996dd8da6973c4d72
nikkinotsixx <nikita.veremeev@gmail.com>
2018-11-30 11:58:09 +0300

    Исправление замечаний

--- Lesson_2/date.rb
+++ Lesson_2/date.rb
@@ -1,13 +1,13 @@
-print "Введите число: "
+print 'Введите число: '
 date = gets.to_i
-print "Введите номер месяца: "
+print 'Введите номер месяца: '
 month = gets.to_i
-print "Введите год: "
+print 'Введите год: '
 year = gets.to_i
 
 days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-if year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+if year % 4 == 0 && year % 100 != 0 || year % 400 == 0
   days_in_month[1] += 1
 end
 
--- Lesson_2/goods.rb
+++ Lesson_2/goods.rb
@@ -2,14 +2,14 @@ goods = {}
 total = 0
 
 loop do
-  print "Введите название товара или слово стоп для завершения ввода: "
+  print 'Введите название товара или слово стоп для завершения ввода: '
   item = gets.chomp
-  break if item == "стоп"
-  print "Введите цену товара: "
+  break if item.downcase == 'стоп'
+  print 'Введите цену товара: '
   price = gets.to_f
-  print "Введите количество: "
+  print 'Введите количество: '
   quantum = gets.to_i
-  goods[item] = {price: price, quantum: quantum}
+  goods[item] = { price: price, quantum: quantum }
 end
 
 goods.each do |type, value|
@@ -18,4 +18,4 @@ goods.each do |type, value|
   print "Итоговая цена за #{type} = #{item_total}"
 end
 
-print "Итоговая цена за все товары = #{total}"
+puts "Итоговая цена за все товары = #{total}"
